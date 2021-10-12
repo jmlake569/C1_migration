4b7418b4f207da4241e1653b43bc859d1b7f7558
jmlake569 <37003520+jmlake569@users.noreply.github.com>
2021-10-12 18:15:00 -0400

    Updated A1_C1.ps1
    Added try/catch error handing in case file is missing

--- A1_C1.ps1
+++ A1_C1.ps1
@@ -1,48 +1,51 @@
-#Place this script into a task scheduler on Windows with "Highest priveleges" and have the task start on startup/login depending how you want this to run. 
-#You can also just execute this script at anytime on a machine as long as you are executing out of the directory. 
-#This script will automatically identify what is installed and what needs to be removed. The Script has ONE goal and thats to remove anything with Apex One and install Cloud One.
+#place this script into a task scheduler on Windows with "Highest priveleges" and have the task start on startup/login depending how you want this to run. 
+#you can also just execute this script at anytime on a machine as long as you are executing out of the directory. 
+#this script will automatically identify what is installed and what needs to be removed. The Script has ONE goal and thats to remove anything with Apex One and install Cloud One.
 
 #SCUT tool and Cloud One install script must all exist in the same folder/directory when running this script
 
-#Registry key paths to verify if Apex One is installed and/or Cloud One is installed
+#registry key paths to verify if Apex One is installed and/or Cloud One is installed
 $arguments = "-noinstall -dbg"
 $cloudOneExists = Test-Path "HKLM:\SOFTWARE\TrendMicro\Deep Security Agent"
 $apexExists = Test-Path "HKLM:\SOFTWARE\TrendMicro\Osprey"
 $logfilepath = "logs.txt"
-#if Apex One exists this will call our SCUT tool to remove Apex One then force a restart and the script will start the Cloud One install
 
 Start-Transcript -Path $logfilepath
 
-Write-Host "Checking for elevated permissions..."
+Write-Host "Checking for elevated permissions..." -ForegroundColor Green
 if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
 Write-Warning "Insufficient permissions to run this script. Open the PowerShell console as an administrator and run this script again."
 break
 }
-
-if ($apexExists -eq $true){
+if ($apexExists -eq $true) {
     Write-Host "Removing Apex One" -ForegroundColor Green
+try {
     $p = Start-Process ".\SCUT.exe" -ArgumentList $arguments -wait -NoNewWindow -PassThru
     $p.HasExited
     $p.ExitCode
-   
-        if ($? -eq $true) {
-        Write-Host "SCUT Succeeded - Forcing Restart" - ForegroundColor Green
-	Restart-Computer -Force
-        }
-        else {
-        Write-Host "SCUT Failed - Please Check Error logs"
-        $LastExitCode
-        }
+ 
+}catch { 
+  # oops remove-item failed. Write warning then quit 
+  # replace the following with what you want to do
+  write-warning "Start-Process encounter error: $_"
+  Return # script failed
+}
+if ($? -eq 0) {
+    Write-Host "SCUT Succeeded - Forcing Restart" -ForegroundColor Green
+    Restart-Computer -Force
+}else {
+    Write-Warning "SCUT Failed - Please Check Error logs"
+    }
 }
 #If Cloud One is already installed it will give warning then close the script
-if ($cloudOneExists -eq $true){
-    Write-Host "Cloud One Is Already Installed" -ForegroundColor Red
+if ($cloudOneExists -eq $true) {
+    Write-Warning "Cloud One Is Already Installed" -ForegroundColor Red
     Start-Sleep -Seconds 3
-    Write-Host "Closing Program" -ForegroundColor Red
+    Write-Host "Exiting..." -ForegroundColor Red
     Start-Sleep -Seconds 1
 }
 #If CloudOne doesn't exist it will begin the install using the script provided in the same directory as this script - no restart is required for Cloud One agents 20+
-if ($cloudOneExists -eq $false -and $apexExists -eq $false){
+if ($cloudOneExists -eq $false -and $apexExists -eq $false) {
     Write-Host "Starting Cloud One Agent Install" -ForegroundColor Blue
     Invoke-Expression ".\C1_install.ps1"
 }
